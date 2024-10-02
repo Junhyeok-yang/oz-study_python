ba05a27361701ae0d59cc27bd58761361d95da79
Junhyeok_yang <ygs881857@gmail.com>
2024-10-02 18:44:37 +0900

    모듈

---
+++ 7th/mymath.py
@@ -0,0 +1,24 @@
+def reimbursable_input():
+    value = input("급여 금액을 입력하세요.")
+    return int(value)
+
+def non_reimbursable_input():
+    value = input("비급여 금액을 입력하세요.")
+    return int(value)
+
+def reimbursable(reimbursable_money):
+    apply_deduction = reimbursable_money * 0.1
+    if apply_deduction > 10000:
+        return apply_deduction
+    else:
+        return 10000
+    
+def non_reimbursable(non_reimbursable_money):
+    apply_deduction = non_reimbursable_money * 0.2
+    if apply_deduction > 10000:
+        return apply_deduction
+    else:
+        return 10000
+    
+def total_deduction():
+    return non_reimbursable + reimbursable
